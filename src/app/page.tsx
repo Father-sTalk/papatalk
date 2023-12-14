c83666c80026af3c641db6b3c8be4791449b784e
jungbam <jungbam0809@gmail.com>
2023-12-15 01:45:55 +0900

    feat: add icon component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 import React from "react";
 
-import Check from "@/assets/icon/check";
+import ChatIcon from "@/assets/icon/chat";
+import CheckIcon from "@/assets/icon/check";
+import SortIcon from "@/assets/icon/sort";
+import CheckBoxIcon from "@/assets/icon/checkBox";
+import RadioSelectIcon from "@/assets/icon/radioSelect";
+import LogoutIcon from "@/assets/icon/logout";
+import ThumbsUpIcon from "@/assets/icon/thumbsup";
 
 export default function Home() {
-  const [slected, setSlected] = React.useState(false);
+  const [selected, setSlected] = React.useState(false);
   const onClickHandler = () => {
-    setSlected(!slected);
+    setSlected(!selected);
   };
   return (
     <div className="w-w-smallBox h-h-header bg-fg-focus">
-      <Check selected={slected} />
+      <LogoutIcon />
+      <SortIcon sort={selected ? "asc" : "desc"} />
+      <ChatIcon selected={selected} />
+      <CheckIcon selected={selected} />
+      <CheckBoxIcon checked={selected} size={{ width: 200, height: 200 }} />
+      <RadioSelectIcon checked={selected} size={{ width: 30, height: 30 }} />
+      <ThumbsUpIcon up={selected} />
       <button onClick={onClickHandler}>클릭</button>
     </div>
   );
---
+++ src/assets/config/iconColor.ts
@@ -0,0 +1,5 @@
+export const iconColor = {
+  primary: "#333333",
+  selected: "#F99DE2",
+  unselected: "#757575",
+};
