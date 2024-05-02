7c2edb03efc4087845644771bcf78aaa4baf84ee
jungbam <jungbam0809@gmail.com>
2024-05-02 22:51:20 +0900

    fix: change root path

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://papatalk.vercel.app/home",
+  siteUrl: "https://papatalk.vercel.app",
   generateRobotsTxt: true,
 };
