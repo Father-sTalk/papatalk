7ae0ca995e4a1fd11f3f4b78ec411a573f78118e
jungbam <jungbam0809@gmail.com>
2024-06-27 00:47:30 +0900

    chore: delete unused code

--- src/models/hooks/community/get_communityList.hooks.tsx
+++ src/models/hooks/community/get_communityList.hooks.tsx
@@ -6,7 +6,6 @@ import communityAPI from "@/models/api/communityAPI";
 const useGetCommunityList = (
   query: ApiRequest.CommunityApiTypes.GetArticleListRequest,
 ) => {
-  console.log(query);
   return useQuery({
     queryKey: [queryKey.COMMUNITY_LIST, query],
     queryFn: () => communityAPI.getCommunityList(query).then((res) => res.data),
